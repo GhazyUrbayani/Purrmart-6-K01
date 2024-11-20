e5d7d4ceac3d245e778da4846ea71ff9132c16d8
GhazyUrbayani <163674292+GhazyUrbayani@users.noreply.github.com>
2024-11-20 22:07:29 +0700

    Create liststatuser.c

---
+++ src/ADT/List Statis/liststatuser.c
@@ -0,0 +1,32 @@
+#include "liststatisuser.h"
+
+/* Membuat list statis kosong */
+void CreateListStatUser(ListStatUser *l) {
+    l->neff = 0;
+}
+
+/* Mengecek apakah list kosong */
+boolean IsEmptyUser(ListStatUser l) {
+    return l.neff == 0;
+}
+
+/* Mengecek apakah list penuh */
+boolean IsFullUser(ListStatUser l) {
+    return l.neff == CAPACITY;
+}
+
+/* Menambahkan elemen baru di akhir list */
+void InsertLastUser(ListStatUser *l, ElType val) {
+    if (!IsFullUser(*l)) {
+        l->buffer[l->neff] = val;
+        l->neff++;
+    }
+}
+
+/* Menghapus elemen terakhir dari list */
+void DeleteLastUser(ListStatUser *l, ElType *val) {
+    if (!IsEmptyUser(*l)) {
+        *val = l->buffer[l->neff - 1];
+        l->neff--;
+    }
+}

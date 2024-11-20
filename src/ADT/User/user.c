0a376d8be92433d5d4a46c1d989ae6c3fef1c686
GhazyUrbayani <163674292+GhazyUrbayani@users.noreply.github.com>
2024-11-21 02:06:21 +0700

    Create user.c

--- src/ADT/User/user.c
+++ src/ADT/User/user.c
@@ -1,21 +1,28 @@
 #include "user.h"
-#include <string.h>
+#include "../mesinkata.h" // Include MesinKata untuk fungsi salinan string
 
 /* Membuat User baru */
 void CreateUser(User *u, char name[], char password[], int money) {
-    strcpy(u->name, name);
-    strcpy(u->password, password);
+    CopyString(u->name, name);       // Menggunakan fungsi dari MesinKata
+    CopyString(u->password, password); // Menggunakan fungsi dari MesinKata
     u->money = money;
 }
 
 /* Mengecek apakah dua User sama berdasarkan nama */
 boolean IsSameUser(User u1, User u2) {
-    return strcmp(u1.name, u2.name) == 0;
+    int i = 0;
+    while (u1.name[i] != '\0' && u2.name[i] != '\0') {
+        if (u1.name[i] != u2.name[i]) {
+            return false;
+        }
+        i++;
+    }
+    return u1.name[i] == '\0' && u2.name[i] == '\0';
 }
 
 /* Menghapus data User */
 void DeleteUser(User *u) {
-    strcpy(u->name, "");       // Mengosongkan nama
-    strcpy(u->password, "");   // Mengosongkan password
-    u->money = 0;              // Mengosongkan saldo
+    u->name[0] = '\0';      // Mengosongkan nama
+    u->password[0] = '\0';  // Mengosongkan password
+    u->money = 0;           // Mengosongkan saldo
 }

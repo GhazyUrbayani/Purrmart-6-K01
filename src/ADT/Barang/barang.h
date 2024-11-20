ea46ddb85e575353399f9eaab009c2060a045c6d
GhazyUrbayani <163674292+GhazyUrbayani@users.noreply.github.com>
2024-11-21 01:58:14 +0700

    Create barang.h

---
+++ src/ADT/Barang/barang.h
@@ -0,0 +1,37 @@
+#ifndef BARANG_H
+#define BARANG_H
+
+#include "../boolean.h"
+
+#define MAX_LEN 50 // Panjang maksimum untuk nama barang
+
+typedef struct {
+    char name[MAX_LEN]; // Nama barang
+    int price;          // Harga barang
+} Barang;
+
+/* *** Konstruktor *** */
+/**
+ * Membuat Barang baru dengan atribut yang diberikan.
+ * IS: Barang belum terinisialisasi.
+ * FS: Barang terinisialisasi dengan nama dan harga.
+ */
+void CreateBarang(Barang *b, char name[], int price);
+
+/* *** Fungsi dan Operasi *** */
+/**
+ * Mengecek apakah dua Barang sama berdasarkan nama.
+ * IS: Kedua Barang valid.
+ * FS: Tidak ada perubahan.
+ * Return: true jika nama sama, false jika tidak.
+ */
+boolean IsSameBarang(Barang b1, Barang b2);
+
+/**
+ * Menghapus data Barang dengan mengosongkan atribut-atributnya.
+ * IS: Barang valid dan terinisialisasi.
+ * FS: Atribut Barang di-set menjadi kosong atau default.
+ */
+void DeleteBarang(Barang *b);
+
+#endif

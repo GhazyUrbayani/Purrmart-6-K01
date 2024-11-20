612142ff692e3c6f1fa03df27216a76a1d09c2dd
GhazyUrbayani <163674292+GhazyUrbayani@users.noreply.github.com>
2024-11-21 01:55:26 +0700

    Create user.h

---
+++ src/ADT/User/user.h
@@ -0,0 +1,38 @@
+#ifndef USER_H
+#define USER_H
+
+#include "../boolean.h"
+
+#define MAX_LEN 50 // Panjang maksimum untuk nama dan password
+
+typedef struct {
+    char name[MAX_LEN];      // Nama pengguna
+    char password[MAX_LEN];  // Password pengguna
+    int money;               // Jumlah uang pengguna
+} User;
+
+/* *** Konstruktor *** */
+/**
+ * Membuat User baru dengan atribut yang diberikan.
+ * IS: User belum terinisialisasi.
+ * FS: User terinisialisasi dengan nama, password, dan uang.
+ */
+void CreateUser(User *u, char name[], char password[], int money);
+
+/* *** Fungsi dan Operasi *** */
+/**
+ * Mengecek apakah dua User sama berdasarkan nama.
+ * IS: Kedua User valid.
+ * FS: Tidak ada perubahan.
+ * Return: true jika nama sama, false jika tidak.
+ */
+boolean IsSameUser(User u1, User u2);
+
+/**
+ * Menghapus data User dengan mengosongkan atribut-atributnya.
+ * IS: User valid dan terinisialisasi.
+ * FS: Atribut User di-set menjadi kosong atau default.
+ */
+void DeleteUser(User *u);
+
+#endif

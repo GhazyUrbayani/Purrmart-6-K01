92aee58e105c380b918ca6111dcad2c8cfd4cd24
mabismaramadhan <18221028@std.stei.itb.ac.id>
2024-11-24 05:41:49 +0700

    add tebakAngka

---
+++ src/tebakAngka.h
@@ -0,0 +1,12 @@
+#ifndef TEBAKANGKA_H
+#define TEBAKANGKA_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "boolean.h"
+#include "mesinkarakter.h"
+#include "mesinkata.h"
+
+int tebakAngka();
+
+#endif

8a469e8b771469ebab30aaf3a063a37692125bc4
Inggrieda <163884726+Inggrieda@users.noreply.github.com>
2024-11-23 15:09:00 +0700

    Create logout.h

---
+++ src/command/logout.h
@@ -0,0 +1,12 @@
+#ifndef logout_H
+#define logout_H
+
+#include "boolean.h"
+#include "liststatususer.h"
+#include "user.h"
+#include "mesinkata.h"
+
+// User melakukan logout
+void logout(User* user);
+
+#endif

92bf6c2a65f4a29feb87df237122ce7cfc28234b
snoeriza <19623022@std.stei.itb.ac.id>
2024-12-18 10:57:41 +0700

    fix : wishlist-swap header

---
+++ src/Wishlist/Wishlist-swap.h
@@ -0,0 +1,10 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include "../ADT/linkedList.h"
+
+/**
+ * Melakukan pertukaran elemen pada wishlist pada indeks idx1 dan idx2.
+ * IS: wishlist terdefinisi
+ * FS: wishlist[idx1] dan wishlist[idx2] ditukar.
+ */
+void wishlistswap (LinkedList* wishlist, int idx1, int idx2);

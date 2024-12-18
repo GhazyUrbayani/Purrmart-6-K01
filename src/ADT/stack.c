a80b0582988578300959d5adaffff353bc40d9f5
GhazyUrbayani <163674292+GhazyUrbayani@users.noreply.github.com>
2024-12-18 23:46:21 +0700

    Stack
    draft

---
+++ src/ADT/stack.c
@@ -0,0 +1,53 @@
+#include "stack.h"
+
+void CreateStack(Stack *S)
+{
+    Top(*S) = Nil;
+}
+
+boolean IsStackEmpty(Stack S)
+{
+    return Top(S) == Nil;
+}
+
+boolean IsStackFull(Stack S)
+{
+    return Top(S) == MaxEl - 1;
+}
+
+void Push(Stack *S, infotypeStack X, boolean *succeed)
+{
+    if (IsStackEmpty(*S))
+    {
+        Top(*S) += 1;
+        InfoTop(*S) = X;
+        (*succeed) = true;
+    }
+    else
+    {
+        if (X < InfoTop(*S))
+        {
+            Top(*S) += 1;
+            InfoTop(*S) = X;
+            (*succeed) = true;
+        }
+        else
+        {
+            (*succeed) = false;
+        }
+    }
+}
+
+void Pop(Stack *S, infotypeStack *X, boolean *succeed)
+{
+    if (!IsStackEmpty(*S))
+    {
+        (*X) = InfoTop(*S);
+        Top(*S) -= 1;
+        (*succeed) = true;
+    }
+    else
+    {
+        (*succeed) = false;
+    }
+}

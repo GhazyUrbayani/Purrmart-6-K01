8bccf62d5ded2bd69ba4800edd01dfdc3ec2bdc9
snoeriza <19623022@std.stei.itb.ac.id>
2024-12-19 17:36:19 +0700

    feat: add wishlist-add

---
+++ src/Wishlist/Wishlist-add.h
@@ -0,0 +1,10 @@
+#ifndef WISHLIST_ADD_H
+#define WISHLIST_ADD_H
+# include <stdio.h>
+# include "../ADT/linkedList.h"
+# include "../ADT/mesinkata/mesinkata.h"
+# include "../ADT/listdinamis.h"
+
+void wishlistadd (LinkedList* wishlist, DList list_barang);
+
+#endif
